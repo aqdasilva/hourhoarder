3fa8af992528a92d891312ef0c1e80e498db87ce
aqdasilva <antqdasilva@gmail.com>
2023-06-26 19:34:52 -0500

    starting page

---
+++ src/Components/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Fullscreen } from '@mui/icons-material';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'relative',
+    },
+    gradientBackground: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '10px',
+      background: 'linear-gradient(to bottom right, #1e88e5, #1976d2)',
+      zIndex: -1,
+    },
+    square: {
+      width: '800px',
+      height: '600px',
+      borderRadius: '10px',
+      backgroundColor: '#FFC0CB',
+    },
+  }));
+
+const GradientSquare = () => {
+    const classes = useStyles();
+  
+    return (
+        <div className={classes.container}>
+          <div className={classes.gradientBackground} />
+          <div className={classes.square} />
+        </div>
+    ); 
+};
+
+const Main = () => {
+  return (
+    <div>
+      <GradientSquare />
+    </div>
+  )
+}
+
+export default Main;
